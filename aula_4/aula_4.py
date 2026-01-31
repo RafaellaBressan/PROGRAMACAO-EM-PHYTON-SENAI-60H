e909ad7e4ebeadd8d47f8ba299650b4954933c1c
RafaellaBressan <rafaellabressandacosta14@gmail.com>
2026-01-31 16:10:46 -0300

    Add files via upload

---
+++ aula_4/aula_4.py
@@ -0,0 +1,43 @@
+print('teste')
+
+#ATIVIDADES:
+
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+#2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+
+#4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+
+
+#nome e sobrenome
+
+nome = 'Rafaella'
+
+sobrenome = 'Bressan'
+
+print (nome , sobrenome)
+
+
+#dois números
+
+n1 = input('Digite um número inteiro: ')
+
+n2 = input('Digite um número inteiro: ')
+
+print ('Números digitados:', n1, n2)
+
+
+#palavra python + número
+
+palavra = 'Python'
+
+#frase + palavra do usuário
+frase = 'A pressa é inimiga da perfeição'
+palavra_usuario = input('Digite uma palavra: ')
+
+((palavra + n1))
+print (frase, palavra_usuario)
