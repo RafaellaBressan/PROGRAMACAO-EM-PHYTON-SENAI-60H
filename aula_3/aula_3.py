bcc8eb5ec309ab4ce23306e4ff78b89b827829e6
RafaellaBressan <rafaellabressandacosta14@gmail.com>
2026-01-31 12:57:58 -0300

    Add files via upload

---
+++ aula_3/aula_3.py
@@ -0,0 +1,24 @@
+# Esses são dados do tipo strig
+
+'Isso é um texto'
+"Isso também é um texto"
+print('teste')
+
+# Esses são dados do tipo inteiro
+10
+20
+30
+print(10)
+
+# Esses são dados do tipo real
+
+10.5
+5.2
+1.10
+print(5.2)
+
+# Esse são dados boleanos
+
+True
+False
+print(True)
---
+++ aula_3/dados primitivos.py
@@ -0,0 +1,9 @@
+print(type('teste'))
+
+print(type(10))
+
+print(type(5.2))
+
+
+print(type(True))
+print(type(False))
