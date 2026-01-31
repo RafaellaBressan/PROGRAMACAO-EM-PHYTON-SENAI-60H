f099841f9d313ed4a2cc4b9f478926bb3d18d2cf
RafaellaBressan <rafaellabressandacosta14@gmail.com>
2026-01-31 16:53:22 -0300

    Add files via upload

---
+++ aula_5/aula_5.py
@@ -0,0 +1,40 @@
+#mensagem de boas-vindas na tela
+
+mensagem = 'Boas-vindas'
+
+print ('Boas-vindas')
+
+
+#declare uma variável boleana verdadeiro com o valor true e imprima seu tipo
+
+d = True
+print (type(d))
+
+
+#imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+n1 = 1.5
+n2 = 0.25
+
+print (n1 * n2)
+
+#imprima o resultado da divisão (/) de dois números inteiros de sua escolha
+
+n3 = 20
+n4 = 30
+
+print (n3/n4)
+
+#imprima o resultado da subtração de dois números inteiros de sua escolha
+
+n5 = 80
+n6 = 40
+
+print (n5 - 54)
+
+#imprima o resultado da divisão (//) inteira de dois números inteiros de sua escolha
+
+n7 = 90
+n8 = 10
+
+print (n7//n8)
