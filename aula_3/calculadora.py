2236e16e78e0ab7253b38f7561c00ff4b46d2987
RafaellaBressan <rafaellabressandacosta14@gmail.com>
2026-01-31 14:35:26 -0300

    Add files via upload

---
+++ aula_3/calculadora.py
@@ -0,0 +1,16 @@
+# CALCULADORA
+
+n1 = float( input('Digite o primeiro número') )
+n2 = float( input('Digite o segundo número') )
+n3 = float( input('Digite o terceiro número') )
+n4 = float( input('Digite o quarto número') )
+
+soma  =  n3 + n2 
+subtracao = n1 - n4
+divisao = n2 / n4
+multiplicacao = n1 * n3
+                  
+print('soma: ', soma)
+print('subtração: ', subtracao)
+print('divisão: ', divisao)
+print('multiplicação: ', multiplicacao)
